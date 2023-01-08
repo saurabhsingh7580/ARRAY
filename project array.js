98cfba6f0962e728606cb1a9d36805f61f164a01
saurabh <singhsaurabh758082@gmail.com>
2023-01-08 23:51:43 +0530

    some more file add

---
+++ project array.js
@@ -0,0 +1,97 @@
+// 1 isArra
+
+// let arr =[1,2,4,0,]
+// let b =Array.isArray(arr);
+// console.log(b)
+
+
+//  3 get first
+
+ 
+// function value(){
+//     let arr =[7,9,0,-2,]
+//  }
+//  console.log(value()[0])
+// 4 get last
+
+// let arr =[7,9,0,-2]
+// console.log(arr[3])
+
+// 5 join all elements
+
+// let arr = ["red","green","white","black"]
+//  let b = arr.join("+")
+// console.log(b)
+
+
+// 6 join all elements
+
+// let arr = [0,254,6,8]
+//  let b = arr.join("-")
+// console.log(b)
+
+
+// 7 sort
+
+// let arr =[3,8,7,6,5,-4,3,2,1]
+//  arr.sort()
+// console.log(arr)
+
+
+// 8 pata karne kai liya yaha kitane bar aya hai
+
+
+
+// let arr = [ 3,'a','a','a',2,3,'a',2,4,9,3]
+// let b ;
+// for(var i =0;i<arr.length;i++){
+    
+// }
+
+
+// function value(math, science,english,hindi,account){
+//     var no = math+ science+english+hindi+account
+//     return no ;
+// }
+// function percrntage(tt){
+//     var per=tt/5;
+//     console.log(per)
+// }
+// var total=value(90,90,80,80,80)
+// percrntage(total)
+
+// function name(fname,lname){
+//     let arr = [ 3,'a','a','a',2,3,'a',2,4,9,3]
+    
+
+// }
+
+// var a= [
+
+//         [2,4,5,6,2,],
+//         [2,4,6,7,8],
+//         [4,5,6,8,9]
+// ]
+// var row=0;
+// for(var i=0;i<a.length;i++){
+//     document.write(`<h3>row${row}</h3>`)
+//     for(var j =0 ;j<a.length[i];j++){
+//         document.write(`<h3>${a[i][j]}</h3>`)
+//     }
+//     row++;
+// }
+
+
+// var arr = [ 2,254,6,7]
+// for (var i =0 ; i<arr.length;i++){
+//     if(i==arr.length)
+//     break;
+//     else if(arr[i]%2==0)
+//    document.write(arr[i]+"-");
+//     else
+//    document.write(`${arr[i]}`)
+// }
+
+
+
+
